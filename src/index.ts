d1ad8baff23b28e0f4a9ed9f7d46084e1e06149f
selim2066 <mdselimreza2066@gmail.com>
2026-05-10 05:12:28 +0600

    fix: remove trailing slash from CORS origin URL

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://frontend-al-quran-web-app.vercel.app/", // update after deploying
+      "https://frontend-al-quran-web-app.vercel.app", // update after deploying
     ],
   }),
 );
