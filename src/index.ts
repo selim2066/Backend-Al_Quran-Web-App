c0506c8b277a0b4a98d20dbef39c5ca0a338f215
selim2066 <mdselimreza2066@gmail.com>
2026-05-10 04:26:29 +0600

    refactor: update server initialization and logging format

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
-import express from "express";
+// import express from "express";
+// import cors from "cors";
+// import surahsRouter from "./routes/surahs";
+// import ayahsRouter from "./routes/ayahs";
+// import searchRouter from "./routes/search";
+
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:3000",
+//     "https://your-frontend.vercel.app", // update after deploying
+//   ],
+// }));
+// app.use(express.json());
+
+// app.use("/api/surahs", surahsRouter);
+// app.use("/api/ayahs", ayahsRouter);
+// app.use("/api/search", searchRouter);
+
+// app.get("/", (_req, res) => {
+//   res.json({ status: "ok", message: "Quran API — Node.js + Express" });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// });
+
+// !v2
 import cors from "cors";
-import surahsRouter from "./routes/surahs";
+import express from "express";
 import ayahsRouter from "./routes/ayahs";
 import searchRouter from "./routes/search";
+import surahsRouter from "./routes/surahs";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://your-frontend.vercel.app", // update after deploying
-  ],
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-frontend.vercel.app", // update after deploying
+    ],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/surahs", surahsRouter);
@@ -23,6 +55,6 @@ app.get("/", (_req, res) => {
   res.json({ status: "ok", message: "Quran API — Node.js + Express" });
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Alhamdulillah Server running on http://localhost:${PORT}`);
 });
